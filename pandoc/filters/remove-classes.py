70feb9b8750607fc2beb1592e13c02e48f13205e
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2026-02-10 21:40:56 +0100

    Remove divs with class "html" and "online", whose contents should be kept

--- pandoc/filters/remove-classes.py
+++ pandoc/filters/remove-classes.py
@@ -5,13 +5,19 @@ Panflute filter: remove classes that are not supposed to be in the html
 
 import panflute as pf
 
-CLASSES_TO_REMOVE = "TODO latex pdf".split()
+# The following <div>s should be removed, and all their content:
+CONTENT_TO_REMOVE = "TODO latex pdf".split()
+
+# The following <div>s should be removed, but not their content:
+CONTENT_TO_KEEP = "html online".split()
 
 
 def remove_classes(elem, doc):
     for cls in getattr(elem, 'classes', ()):
-        if cls in CLASSES_TO_REMOVE:
+        if cls in CONTENT_TO_REMOVE:
             return []
+        if cls in CONTENT_TO_KEEP:
+            return list(elem.content)
 
 
 def main(doc = None):
